29a4c120d3f23f162e2676406ad1b3ee7dbc6717
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-11-25 13:04:14 +0200

    Assignment, Incriment and Decrement

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,15 @@
 //Comparison operators (==, !=, <, >, <=, >=)
 fun main() {
-val isEquals = 5 == 3
-    println("isEqual is $isEquals")
-    val isNotEquals = 5!=5;
-    println("isNotEquals is $isNotEquals")
-
-    println("is5Greater3 ${5 > 3}")
-    println("is5LowerEquals3 ${5 <= 3}")
-    println("is5GreaterEqual3 ${5 > 3}")
-
+var myNum = 5
+//    myNum = myNum + 3
+    myNum += 3 // 8
+    myNum *= 4 // 32
+    println("myNum is $myNum") // 32
+// Increment & decrement operators (++, --)
+    myNum++; // 33
+    println("myNum is $myNum") // print 33
+    println("myNum is ${myNum++}") // 33
+    // myNum++ before the next line (+1) = 34
+    println("myNum is ${++myNum}") // (+1) 35
+    println("myNum is ${--myNum}") // 34
 }
