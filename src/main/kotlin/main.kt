9fdb551381257cb8bc56b381c010b0d468ae3af5
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-11-25 14:21:54 +0200

    If, Else, Else If Statements

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,33 @@
-//Comparison operators (==, !=, <, >, <=, >=)
+// If, Else, Else If Statements
 fun main() {
-var myNum = 5
-//    myNum = myNum + 3
-    myNum += 3 // 8
-    myNum *= 4 // 32
-    println("myNum is $myNum") // 32
-// Increment & decrement operators (++, --)
-    myNum++; // 33
-    println("myNum is $myNum") // print 33
-    println("myNum is ${myNum++}") // 33
-    // myNum++ before the next line (+1) = 34
-    println("myNum is ${++myNum}") // (+1) 35
-    println("myNum is ${--myNum}") // 34
+//val heightPerson1 = 170
+//val heightPerson2 = 189
+//    if (heightPerson1 > heightPerson2) {
+//        println("use raw force")
+//    } else if (heightPerson1 == heightPerson2) {
+//        println("are equal")
+//    }
+//    else {
+//        println("use technique")
+//    }
+//}
+//    val age = 18
+//    if (age <= 20) {
+//        println("Not enough")
+//    } else if (age >= 20) {
+//        println("Enough")
+//    } else {
+//        println("You know, I don't know!")
+//    }
+
+    var name = "Leo"
+    if (name == "Leo") {
+        println("You're welcome!")
+    } else {
+        println("Who are you?")
+    }
+    var isRainy = true;
+    if (isRainy) {
+        println("It's rainy")
+    }
 }
