5dea5780c5eebae95e0a7c83ec128da7f5edf9f1
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-11-26 13:20:21 +0200

    When Expressions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,33 +1,36 @@
-// If, Else, Else If Statements
+// "When" Expressions
 fun main() {
-//val heightPerson1 = 170
-//val heightPerson2 = 189
-//    if (heightPerson1 > heightPerson2) {
-//        println("use raw force")
-//    } else if (heightPerson1 == heightPerson2) {
-//        println("are equal")
-//    }
-//    else {
-//        println("use technique")
-//    }
-//}
-//    val age = 18
-//    if (age <= 20) {
-//        println("Not enough")
-//    } else if (age >= 20) {
-//        println("Enough")
-//    } else {
-//        println("You know, I don't know!")
-//    }
-
-    var name = "Leo"
-    if (name == "Leo") {
-        println("You're welcome!")
-    } else {
-        println("Who are you?")
+    var season = 3
+    when (season) {
+        1 -> println("Spring")
+        2 -> println("Summer")
+        3 -> {
+            println("Fall")
+            println("Autumn")
+        }
+        4 -> println("Winter")
+        else -> println("Invalid Season")
     }
-    var isRainy = true;
-    if (isRainy) {
-        println("It's rainy")
+    var month = 3
+    when (month) {
+        in 3..5 -> println("Spring")
+        in 6..8 -> println("Summer")
+        in 9..11 -> println("Autumn")
+        12, 1, 2 -> println("Winter")
+        else -> println("Invalid season")
+    }
+    var age = 18
+    when (age) {
+        !in 0..20 -> println("now you may drink in the US")
+        in 18..20 -> println("you may vote now")
+        16, 17 -> println("you may drive now")
+        else -> println("you're too young")
+    }
+    var x : Any = "13.37f"
+    when(x) {
+        is Int -> println("$x is an Int")
+        !is Double -> println("$x is not a Double")
+        is String -> println("$x is a String")
+        else -> println("$x is none of the above")
     }
 }
