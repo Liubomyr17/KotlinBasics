b4357a818d17957feecd1f17b8a2a20500ea6e13
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-11-27 15:36:21 +0200

    Kotlin's Functions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,29 @@
-// "For Loops" Task
-fun main() {
-    var i = 0
-    while(i <= 10000)
-        do {
-            i = 0
-        } while(i == 9001)
-        println("IT'S OVER 9000")
-    var humidity = "humid"
-    var humidityLevel = 80
-    while(humidity == "humid") {
-        humidityLevel -= 5
-        println("humidity decreased")
-        if (humidityLevel < 60) {
-            humidity = "comfy"
-            println("It's comfy now")
-        }
-    }
+// Kotlin's Functions
+// Method - a method is a Function within a class
+    fun main() {
+    // Argument
+    val result = addUp(5, 3)
+    val output = takeOff(5, 3)
+    val avg = avg(22.3, 13.0)
+    println("Result is $result")
+    println("Result is $output")
+    println("Result is $avg")
+    myFunction()
 }
+    // Parameter (input)
+    fun addUp(a: Int, b: Int) : Int {
+    // output
+    return a + b
+    }
+    // Parameter (input)
+    fun takeOff(a: Int, b: Int) : Int {
+    // output
+    return a - b
+    }
+    fun avg(a: Double, b: Double): Double {
+        return (a + b) / 2
+    }
+    fun myFunction() {
+    println("Called from myFunction")
+    }
+
