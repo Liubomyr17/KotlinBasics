c38add2bfb0a892f335a87e87b0232384ab67aaf
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-11-27 13:12:09 +0200

    For Loops Expressions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,26 @@
-// "While Loops" String Expressions
+// "For Loops" Expressions
 fun main() {
-    var feltTemp = "cold"
-    var roomTemp = 10
-    while (feltTemp == "cold") {
-        roomTemp++;
-        if (roomTemp >= 20) {
-            feltTemp = "comfy"
-            println("it's comfy now")
-        }
+    for (num in 1..10) {
+        print("$num")
+        println("__________________")
     }
+
+    for (i in 1 until 10) {
+        print("$i ")
+        println("__________________")
+    }
+
+    for (i in 1.until(10).step(2)) {
+        print("$i ")
+        println("__________________")
+    }
+
+    for (z in 10 downTo 1 step 2) { // Same as - for (z in 10.downTo(1).step(2)
+        print("$z ")
+        println("__________________")
+    }
+
+    for (z in 10.downTo(1).step(2))
+        print("$z ")
+        println("__________________")
 }
