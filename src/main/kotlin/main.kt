e5419fca99b01c8796dffd847cf43c2c6d4140be
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-11-28 10:20:35 +0200

    Classes And Initializers

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,28 +1,16 @@
-// Nullable
+// Classes And Initializers
 
 fun main() {
-    // var name : String = "Liubomyr"
-    // name = null -> Compilation ERROR
-    var nullableName : String? = "Liubomyr"
-    //nullableName = null
-
-    // var len2 = nullableName.length -> ERROR
-    var len2 = nullableName?.length // -> OK
-    println(nullableName?.toLowerCase())
-    nullableName?.let { println(it.length) }
-
-    // ?: -> Elvis operator
-    val name = nullableName ?: "Guest"
-    println("Name is $name")
-    // Output: "Liubomyr"
-    // if nullableName is not commented from the line 7, the output should be "Guest"
-
-    nullableName!!.toLowerCase() // Works only when the nullableName from the line 7 is COMMENTED
-    // Output: "Name is liubomyr"
-
-    //if we comment println("Name is $name")
-    println(nullableName!!.toLowerCase())
-    // Output: "liubomyr"
-
+    var liubomyr = Person("Liubomyr", "Panjuta")
+    var john = Person()
+    var johnPeterson = Person(lastName = "Peterson")
 }
 
+class Person (firstName: String = "John", lastName: String = "Doe") {
+
+    // Initializer block
+    init {
+        println("Initialized a new Person object with " +
+        "firstName = $firstName and lastName = $lastName")
+    }
+}
