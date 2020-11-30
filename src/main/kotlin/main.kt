1f32e87217288fdf9188e7eb69756c9186233bc8
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-11-30 12:48:42 +0200

    Functions And Constructors

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,39 @@
 // Classes And Initializers
 
-var b = 3
 fun main() {
-    myFunction(5)
-    b = 5
+// Creating instances of classes
+    var liubomyr = Person("Liubomyr", "Izdepskyi", 29)
+    liubomyr.hobby = "to skateboard"
+    liubomyr.age = 30
+    println("Liubomyr is ${liubomyr.age} years old")
+    liubomyr.stateHobby()
+    var john = Person()
+    john.hobby = "to play video games"
+    john.stateHobby()
+//    var johnPeterson = Person(lastName = "Peterson")
 }
-// this "a" is a parameter
-fun myFunction(a: Int) {
-/* "a" is a variable
-     var a = a // output should e 5
-     var a = 4 -> output will be 4
-     println("a is $a") */
-    var b = a
-    println("b is $b")
+class Person (firstName: String = "John", lastName: String = "Doe") {
+    // Member Variable - Properties
+    var age : Int? = null
+    var hobby : String = "watch Netflix"
+    var firstName : String? = null
 
+    // Initializer Block
+    init {
+        this.firstName = firstName
+        println("Initialized a new Person object with " +
+                "firstName = $firstName and lastName = $lastName")
+    }
+    // Member secondary Constructor
+    constructor(firstName: String, lastName: String, age: Int)
+    : this(firstName, lastName) {
+        this.age = age
+        println("Initialized a new Person object with " +
+                "firstName = $firstName and lastName = $lastName and age is $age")
+    }
+
+    // Member functions - Methods
+    fun stateHobby() {
+        println("$firstName\'s hobby is $hobby")
+    }
 }
