d3b94f2d3f635e233df93c9713e4b6c7609b5895
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-11-30 14:23:02 +0200

    Lateinit Plus Setters and Getters

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,39 +1,31 @@
-// Classes And Initializers
+// Getters and Setters
 
 fun main() {
-// Creating instances of classes
-    var liubomyr = Person("Liubomyr", "Izdepskyi", 29)
-    liubomyr.hobby = "to skateboard"
-    liubomyr.age = 30
-    println("Liubomyr is ${liubomyr.age} years old")
-    liubomyr.stateHobby()
-    var john = Person()
-    john.hobby = "to play video games"
-    john.stateHobby()
-//    var johnPeterson = Person(lastName = "Peterson")
+var myCar = Car()
+    println("Brand is : ${myCar.myBrand}")
+    myCar.maxSpeed = -5
+    println("Max speed is ${myCar.maxSpeed}")
+    println("Model is ${myCar.myModel}")
 }
-class Person (firstName: String = "John", lastName: String = "Doe") {
-    // Member Variable - Properties
-    var age : Int? = null
-    var hobby : String = "watch Netflix"
-    var firstName : String? = null
+class Car() {
+   lateinit var owner : String
 
-    // Initializer Block
+   val myBrand : String = "BMW"
+       // Custom getter
+    get() {
+        return field.toLowerCase()
+    }
+    var maxSpeed : Int = 250
+    get() = field
+    set(value) {
+        field = if(value > 0) value else throw IllegalArgumentException("Max speed cannot be less than 0")
+    }
+
+    var myModel : String = "M5"
+    private set
+
     init {
-        this.firstName = firstName
-        println("Initialized a new Person object with " +
-                "firstName = $firstName and lastName = $lastName")
-    }
-    // Member secondary Constructor
-    constructor(firstName: String, lastName: String, age: Int)
-    : this(firstName, lastName) {
-        this.age = age
-        println("Initialized a new Person object with " +
-                "firstName = $firstName and lastName = $lastName and age is $age")
-    }
-
-    // Member functions - Methods
-    fun stateHobby() {
-        println("$firstName\'s hobby is $hobby")
+        this.myModel = "M3"
+        this.owner = "Frank"
     }
 }
