1b1cf16c4104aacec3fba9568ce85171a9d85c28
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-12-01 13:29:41 +0200

    Inheritance

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,18 +2,40 @@
 
 
 fun main() {
+var audiA3 = Car("A3", "Audi")
+var teslaS = ElectricCar("S-Model", "Tesla", 85.0)
+    teslaS.chargerType = "Type2"
+    teslaS.extendRange(200.0)
 
-}
-// class Vehicle is a super class or a parent class or base class
-open class Vehicle {
-    // properties
-    // methods
-}
-// class Car is a sub class of class vehicle
-open class Car : Vehicle() {
+    teslaS.drive()
 
+    // Polymorphism
+    audiA3.drive(200.0)
+    teslaS.drive(200.0)
 }
+
+open class Car(val name: String, val brand: String) {
+open var range: Double = 0.0
+
+    fun extendRange(amount: Double) {
+        if (amount > 0)
+            range += amount
+    }
+    open fun drive(distance: Double) {
+        println("Drive for $distance km")
+    }
+}
+
 // class Car is a sub class of class Car
-class ElectricCar: Car() {
+class ElectricCar(name: String, brand: String, batteryLife: Double)
+    : Car(name, brand) {
 
+    var chargerType = "Type1"
+    override var range = batteryLife * 6
+    override fun drive(distance: Double) {
+        println("Drive for $distance km on electricity")
+    }
+       fun drive() {
+           println("Drive for $range km on electricity")
+       }
 }
