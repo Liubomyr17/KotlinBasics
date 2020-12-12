001a5c9d938d1b5ec0c470479a7c09b086ad92da
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-12-12 12:16:28 +0200

    Lambdas 2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,12 @@
-// Lambdas 1
+// Lambdas 2
 
 fun main() {
-val result1 = sum1(2, 3)
-val result2 = sum2(3, 4)
-    println("The sum of two numbers from result1 is: $result1")
-    println("The sum of two numbers from result2 is: $result2")
-// directly print the return value of lambda
-// without storing in a variable
-    println(sum1(5, 7))
+    val result1 = sum1(2, 3)
+    println("The sum of two numbers is: $result1")
 }
-// with type annotation in lambda expression
- val sum1 = {a: Int, b: Int -> a + b}
-
-// without type annotation in lambda expression
-val sum2 : (Int, Int) -> Int = {a, b -> a + b}
+val sum1 = { a: Int, b: Int -> 
+    val num = a + b
+    num.toString() // convert Integer to String
+    // (Int, Int) -> String
+}
 
