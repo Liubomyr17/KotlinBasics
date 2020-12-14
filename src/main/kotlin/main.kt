2632d46a17215c9c042294fe60cb45b3c77f0731
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-12-14 13:44:10 +0200

    Finally try-catch block

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,15 @@
 import java.lang.ArithmeticException
 
-// Example with try-catch block
+// Example with Finally try-catch block
 
 fun main() {
 try {
-    val a = IntArray(5)
-    a[5] = 10 / 0
-} catch (e: ArithmeticException) {
-    println("arithmetic exception catch")
-} catch (e: ArrayIndexOutOfBoundsException) {
-    println("array index out of bounds exception")
-} catch (e: Exception) {
-    println("parent exception class")
+    val data = 10 / 5
+    println(data)
+} catch (e: NullPointerException) {
+    println(e)
+} finally {
+    println("Finally block always executes")
 }
-    println("Code after try catch...")
+    println("Below code...")
 }
